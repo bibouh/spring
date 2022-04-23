cae59a6929718905249fc97c8f00692623b55c76
bibouh <messanfollyh@gmail.com>
2022-04-23 16:39:22 +0000

    premier depot

---
+++ src/main/java/com/istinL3/GestionStock/GestionStockApplication.java
@@ -0,0 +1,13 @@
+package com.istinL3.GestionStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockApplication.class, args);
+	}
+
+}
